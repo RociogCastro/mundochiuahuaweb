9b17850a19179bd705ca7c6fc61062ca0f9790e7
RociogCastro <126022717+RociogCastro@users.noreply.github.com>
2026-04-06 10:51:12 -0300

    Create tipos.ts

---
+++ lib/tipos.ts
@@ -0,0 +1,10 @@
+export type RespuestasFormulario = {
+  nombre: string;
+  whatsapp: string;
+  email?: string;
+  motivo: string;
+  otrosAnimales: string;
+  ninosEdades: string;
+  vivienda: string;
+  experiencia: string;
+};
